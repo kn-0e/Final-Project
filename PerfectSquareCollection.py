9bcaa842475bab1f8bc3d870256734175e843023
kn-0e <96165662+kn-0e@users.noreply.github.com>
2022-01-21 03:46:39 -0800

    Work session 5

--- PerfectSquareCollection.py
+++ PerfectSquareCollection.py
@@ -1,7 +1,24 @@
 import math
+#first I'll try to list out some perfect squares
+# i = 0
+# while i < math.inf:
+#     i +=1 
+#     i**2
 # this is to create a list of three numbers
-# but first I'll try to list out some perfect squares
-i = 0
-while i < math.inf
-    i +=1 
-    print (i**2)
+x = 0
+y = 0 
+z = 0
+l = []
+inf = 1000
+while x < inf:
+    x += 1
+    while y < inf:
+        y += 1
+        while z < inf:
+            z += 1
+            l = [x,x+y,x+y+z]
+            print (l)
+        if z == inf:
+            z = 0
+    if y == inf:
+            y = 0
