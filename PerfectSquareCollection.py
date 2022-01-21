dd8ac3dfcd8e369818e586a3313e88776bdbe998
kn-0e <96165662+kn-0e@users.noreply.github.com>
2022-01-21 04:50:46 -0800

    work session 6
    I've ran this code for almost an hour now and it's not done running haha

--- PerfectSquareCollection.py
+++ PerfectSquareCollection.py
@@ -4,12 +4,35 @@ import math
 # while i < math.inf:
 #     i +=1 
 #     i**2
+
+#this is created to check if i found the numbers
+def check(list):
+    x = list[2]
+    y = list[1]
+    z = list[0]
+    a = x + y
+    b = x - y
+    c = x + z
+    d = x - z
+    e = y + z
+    f = y - z
+    l = [a,b,c,d,e,f]
+    for element in l:
+        element = math.sqrt(element)
+        if not element.is_integer :
+            return False
+    return True
+            
+
+
+
 # this is to create a list of three numbers
+
 x = 0
 y = 0 
 z = 0
 l = []
-inf = 1000
+inf = 10000
 while x < inf:
     x += 1
     while y < inf:
@@ -18,7 +41,12 @@ while x < inf:
             z += 1
             l = [x,x+y,x+y+z]
             print (l)
+            if check(l):
+                break
         if z == inf:
             z = 0
     if y == inf:
             y = 0
+print (l)
+
+
