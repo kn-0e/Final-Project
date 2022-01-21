de93a0689bda2243937fd88056c253b2eabae45e
kn-0e <96165662+kn-0e@users.noreply.github.com>
2022-01-21 00:50:10 -0800

    Work session 3
    I added a few comments and I found the answer to problem 40 on Project Euler. It turns out it's just 210.

--- Champernowne'sconstant.py
+++ Champernowne'sconstant.py
@@ -1,10 +1,19 @@
-def bruteForce(n):
+
+def bruteForce(n): #this bruteforces and finds the number
     b = ''
     for a in range (1,n+1):
         b += str(a)
     return b[n-1]
 
+def mult10(x): # x will be an integer and it will be the exponent for n later on
+    a = 1
+    object = 0
+    for tens in range (x):
+        object = 10**tens
+        a *= int(bruteForce(object))
+    return a 
+
+
 if __name__ == "__main__": 
-    n = 10**6
-    print (bruteForce(n))
+    print (mult10(7))
 
